8d22cd9247c965565cf3434135743d7957b61f4d
vwede oteghele <louisshaw75@gmail.com>
2022-11-22 11:56:41 +0100

    goals controllers added

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+const {errorHandler} = require("./middlewares/errorHandlers")
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+app.use("/api/goals", require("./routes/goalRoute.js"))
+
+app.use(errorHandler);
+
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
